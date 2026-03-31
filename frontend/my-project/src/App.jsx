e0f87a967fb6dc2f20b98c5567c5b682677c3631
hemantsaini30 <hemantsaini9818@gmail.com>
2026-03-31 22:13:35 +0530

    initialise project

---
+++ frontend/my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-pink-900">
+      <h1 className="text-4xl text-white font-bold">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
